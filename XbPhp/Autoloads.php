165d91f3d0ebb50a804499f65a8c90e6f8d136af
mcxiaobo1314 <mcxiaobo1314@qq.com>
2016-04-02 02:28:16 +0800

    修复自动加载机制~

--- XbPhp/Autoloads.php
+++ XbPhp/Autoloads.php
@@ -9,20 +9,21 @@
  * @param string $class_name  文件名
  * @author wave
  */
-function __autoload($class_name) {
-	static  $config = array();
-	if(empty($config) && !isset($config['include'])) {
-		$config['include'] = array(
+function auoload($class_name) {
+	static  $configs = array();
+	if(empty($configs) && !isset($configs['include'])) {
+		$configs['include'] = array(
+			ROOT.DS.ROOT_PATH.DS,
 			ROOT.DS.ROOT_PATH.DS.ROOT_CONTROLLER.DS,
 			ROOT.DS.ROOT_PATH.DS.ROOT_MODEL.DS,
 			ROOT.DS.ROOT_PATH.DS.ROOT_VIEW.DS,
 			ROOT.DS.ROOT_PATH.DS.ROOT_COM.DS,
-			ROOT.DS.ROOT_PATH.DS,
-			ROOT.DS.ROOT_PATH.DS.CACHE.DS
+			ROOT.DS.ROOT_PATH.DS.CACHE.DS,
+
 		);
 	}
-	set_include_path(get_include_path() . PATH_SEPARATOR .implode( PATH_SEPARATOR , $config['include']));
-	include $class_name.'.php';
+	set_include_path(get_include_path() . PATH_SEPARATOR .implode( PATH_SEPARATOR , $configs['include']));
+	require_once $class_name.'.php';
 }
 
 
@@ -31,10 +32,11 @@ function __autoload($class_name) {
  * @author wave
  */
 function AutoloadsStatic() {
-	load('config.php',APP_PATH.DS.DATABASE); //数据库配置文件
+	load('config.php',APP_PATH.DS.DATABASE) ; //数据库配置文件
 	load('Error.php',ROOT_PATH.DS.ROOT_ERROR.DS);					//錯誤
 	load('AppModel.php',APP_PATH.DS.ROOT_MODEL.DS);					//模型
 	load('AppController.php',APP_PATH.DS.ROOT_CONTROLLER.DS);		//控制器
 }
+spl_autoload_register('auoload');
 AutoloadsStatic();
 
