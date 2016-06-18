49f9aedb1772c940d0b800011dee8640c54301e5
mcxiaobo1314 <mcxiaobo1314@qq.com>
2016-06-19 00:22:57 +0800

    优化路由

--- XbPhp/conf/route.php
+++ XbPhp/conf/route.php
@@ -7,7 +7,7 @@
 
 //伪静态路由规则演示,不写则用默认规则
 $route = array(
-	//伪静态路由规则，如:/Index/index/id/1/cid/2,则访问必须要加id/1/cid/2才能进行访问,不然会404
+	//路由规则，如:/Index/index/id/1/cid/2,则访问必须要加id/1/cid/2才能进行访问,不然会404
 	// 'Index'=> array(
 		//	'test' => "/^id\/([a-zA-Z_0-9]+)\/cid\/([a-zA-Z_0-9]+)$/i"
 	//)
